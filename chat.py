f35d148427ab0903b4691db165b8e8da74a1e36d
rohitmalekar <rohitmalekar@gmail.com>
2023-11-16 22:33:17 +0530

    Create chat.py

---
+++ chat.py
@@ -0,0 +1,157 @@
+import streamlit as st
+import os
+
+from langchain.document_loaders import RecursiveUrlLoader
+from langchain.document_transformers import Html2TextTransformer
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores.faiss import FAISS
+from langchain.callbacks import StreamlitCallbackHandler
+from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
+from langchain.agents.agent_toolkits import create_retriever_tool
+from langchain_experimental.agents.agent_toolkits import create_csv_agent
+from langchain.agents.openai_functions_agent.agent_token_buffer_memory import (
+    AgentTokenBufferMemory,
+)
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import SystemMessage, AIMessage, HumanMessage
+from langchain.prompts import MessagesPlaceholder
+from langsmith import Client
+from langchain.agents.agent_types import AgentType
+from langchain.tools import Tool, tool
+from trubrics.integrations.streamlit import FeedbackCollector
+
+client = Client()
+
+os.environ['OPENAI_API_KEY'] = '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'
+
+collector = FeedbackCollector(
+    project="default",
+    email="rohitmalekar@gmail.com",
+    password="aeiour00",
+)
+
+
+st.set_page_config(
+    page_title="GrantsScope",
+    page_icon="🔎",
+    layout="wide",
+    initial_sidebar_state="collapsed",
+)
+
+model="gpt-3.5-turbo-16k"
+
+st.title('GrantsScope - GG19')
+st.markdown('Ask away your questions to learn more about the grantees in the GG19 Climate Round. Information on other rounds coming soon! See useful links in the side bar.')
+col1, col2, col3, col4 = st.columns([1,1,1,1])
+with col1:
+    st.link_button("Explore all projects", "https://explorer.gitcoin.co/#/projects",type="primary")
+#with col2:
+    st.link_button("Suppport GrantsScope", "https://explorer.gitcoin.co/#/round/424/0x98720dd1925d34a2453ebc1f91c9d48e7e89ec29/0x98720dd1925d34a2453ebc1f91c9d48e7e89ec29-195",type="secondary")
+
+
+@st.cache_resource(ttl="1h")
+
+
+def configure_retriever():
+    index = './GG19-climate/storage/faiss_index'
+    embeddings = OpenAIEmbeddings()    
+    vectorstore = FAISS.load_local(index, embeddings)
+    return vectorstore.as_retriever()
+
+summary_tool = create_retriever_tool(
+    configure_retriever(),
+    "Grantee_Discovery",
+    "Helps search information about grantees in various Gitcoin Rounds, use this tool to respond to questions about specific grantees and projects",
+)
+
+
+tools = [summary_tool]
+llm = ChatOpenAI(temperature=0, streaming=True, model=model)
+
+message = SystemMessage(
+    content=(
+        "Do not respond to questions that ask to sort or rank grantees. Do not respond to questions that ask to compare grantees. Similarly, do not respond to questions that ask for advice on which grantee to donate contributions to. Few examples of such questions are (a) Which grantee had the most impact on Gitcoin? (b) Who should I donate to? (c) Rank the grantees by impact (d) Compare work of one grantee versus another? For such questions, do not share any grantee information and just say: ""Dear human, I am told not to influence you with my biases for such queries. The burden of choosing the public greats and saving the future of your kind lies on you. Choose well!"""
+        "If the answer is not available in the context information given above, respond: Sorry! I don't have an answer for this."
+        "Given this information, please answer the following question in detail. When sharing information about a project, share which round they are part of, the website, Twitter and the Explorer Link. Format the output as a table if the response includes multiple grantees."
+    )
+)
+
+prompt = OpenAIFunctionsAgent.create_prompt(
+    system_message=message,
+    extra_prompt_messages=[MessagesPlaceholder(variable_name="history")],
+)
+
+agent = OpenAIFunctionsAgent(llm=llm, tools=tools, prompt=prompt)
+
+agent_executor = AgentExecutor(
+    agent=agent,
+    tools=tools,
+    verbose=True,
+    return_intermediate_steps=True,
+)
+
+
+memory = AgentTokenBufferMemory(llm=llm)
+
+starter_message = "Ask me anything about the grantees in GG19 Climate Round!"
+
+st.sidebar.markdown("## Important Links:")
+
+st.sidebar.markdown("- Check out the new [Explorer](https://explorer.gitcoin.co/#/) with improved search")
+st.sidebar.markdown("- GG19 Donation and Leaderboard [Dashboard](https://gitcoin-grants-51f2c0c12a8e.herokuapp.com/)")
+st.sidebar.markdown("- [Grants Portal](https://grants-portal.gitcoin.co/gitcoin-grants-grantee-portal) for how-to videos, resources, and FAQs")
+st.sidebar.markdown("- Refresh your [Gitcoin Passport](https://passport.gitcoin.co/) score")
+st.sidebar.markdown("- Review the [GG19 Outline and Strategy](https://gov.gitcoin.co/t/gg19-outline-and-strategy/16682)")
+st.sidebar.markdown("- Understand [Gitcoin Vocabulary](https://gov.gitcoin.co/t/vocabulary-gitcoin-grants-programs-rounds-etc/16773)")
+
+
+if "messages" not in st.session_state or st.sidebar.button("Clear message history"):
+    st.session_state["messages"] = [AIMessage(content=starter_message)]
+
+if "logged_prompt" not in st.session_state:
+    st.session_state.logged_prompt = None
+
+def send_feedback(run_id, score):
+    client.create_feedback(run_id, "user_score", score=score)
+
+
+for msg in st.session_state.messages:
+    if isinstance(msg, AIMessage):
+        st.chat_message("assistant").write(msg.content)
+    elif isinstance(msg, HumanMessage):
+        st.chat_message("user").write(msg.content)
+    memory.chat_memory.add_message(msg)
+
+
+if prompt := st.chat_input(placeholder=starter_message):
+    st.chat_message("user").write(prompt)
+    with st.chat_message("assistant"):
+        st_callback = StreamlitCallbackHandler(st.container())
+        
+        response = agent_executor(
+            {"input": prompt, "history": st.session_state.messages},
+            callbacks=[st_callback],
+            include_run_info=True,
+        )
+
+        st.session_state.messages.append(AIMessage(content=response["output"]))
+        st.markdown(response["output"])
+        memory.save_context({"input": prompt}, response)
+        st.session_state["messages"] = memory.buffer
+        run_id = response["__run"].run_id
+
+    st.session_state.logged_prompt = collector.log_prompt(
+        config_model={"model": model},
+        prompt=prompt,
+        generation=response["output"],
+    )
+
+    if st.session_state.logged_prompt:
+        user_feedback = collector.st_feedback(
+            component="GG19",
+            feedback_type="thumbs",
+            open_feedback_label="[Optional] Provide additional feedback",
+            model=model,
+            prompt_id=st.session_state.logged_prompt.id,
+        )
